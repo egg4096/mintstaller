8555f83d469d7e35a6e22a68b5e4dcb70257ebfb
egg4096 <72076491+egg4096@users.noreply.github.com>
2022-03-20 16:16:20 +0000

    Clean

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/linuxmint/mint-x-icons
 git clone https://github.com/linuxmint/mint-y-icons
 
-cp -rf mint-x-icons/usr/share/icons/* ~/.icons
-cp -rf mint-y-icons/usr/share/icons/* ~/.icons
+mv mint-x-icons/usr/share/icons/* ~/.icons
+mv mint-y-icons/usr/share/icons/* ~/.icons
