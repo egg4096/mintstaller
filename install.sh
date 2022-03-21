5ddcc6d59f24292e578d7f1c17d9529ae89275de
egg4096 <72076491+egg4096@users.noreply.github.com>
2022-03-21 16:24:37 +0000

    Makes sure .icons exists

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 git clone https://github.com/linuxmint/mint-x-icons
 git clone https://github.com/linuxmint/mint-y-icons
 
+mkdir -p ~/.icons
 mv mint-x-icons/usr/share/icons/* ~/.icons
 mv mint-y-icons/usr/share/icons/* ~/.icons
